a4c67e4451c9c6215417c4915ddba698ca982b78
Md Maruf Hossain <37552845+ryanmaruf615@users.noreply.github.com>
2021-05-05 02:23:30 +0600

    day 3

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,21 @@
 import express from'express';
 import dotenv from 'dotenv';
-import products from './data/products.js';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import productRoutes from './routes/productRouts.js';
+import { notFound , errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
-
 const app = express();
 
-app.get('/',(req, res)=>{
-    res.send('API IS RUNNING');
-});
+//route
+app.use('/api/products',productRoutes);
 
-app.get('/api/products',(req, res)=>{
-   res.json(products);
-});
+//middlewares
+app.use(notFound);
+app.use(errorHandler);
 
-app.get('/api/products/:id',(req, res)=>{
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
- });
-
- const PORT= process.env.PORT || 5000;
-
+//server port 
+const PORT= process.env.PORT || 5000;
 app.listen(PORT,console.log('server is running'.yellow.bold));
